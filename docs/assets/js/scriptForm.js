bcc0ea07180a0b4497580cebb480c104ed43392e
AleSaJa <alesaja87@gmail.com>
2022-01-28 12:21:05 -0600

    Mejora

--- docs/assets/js/scriptForm.js
+++ docs/assets/js/scriptForm.js
@@ -2,8 +2,11 @@ const btn = document.getElementById('submitBtn');
 const form = document.getElementById('formContacto');
 btn.addEventListener('click', sendEmail);
 
-function sendEmail(){
+
+
+function sendEmail(event){
     try{
+        event.preventDefault();
         Email.send({
         Host : "smtp.mailtrap.io",
         Username : "98e7c94d8e9b4a",
@@ -25,71 +28,3 @@ function sendEmail(){
     }
     
 }
-
-/* 
-//document.getElementById('exampleInputEmail1').addEventListener('input', validar);
-function validar() {
-    // campo = event.target;
-    
-
-    emailRegex = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
-
-
-    //Se muestra un texto a modo de ejemplo, luego va a ser un icono
-    if (emailRegex.test(form['email'].value)) {
-        alert("Correo valido");
-        crear(form['email'].value);
-    } 
-    
-}
-
-
-
-function crear(valueEmail){
-    localStorage.setItem("email", valueEmail)
-}
-
-function sendEmail(name,email,message){
-    email.send({
-        Host:"",
-        Username:
-        Password:
-        To:
-        From:
-        Subject:
-        Body:
-    }).then((message)=> alert())
-}
-
-document.getElementById("crear").addEventListener("click", crear);
-        document.getElementById("mostrar").addEventListener("click", mostrar);
-        document.getElementById("modificar").addEventListener("click", modificar);
-        document.getElementById("eliminar").addEventListener("click", eliminar);
-
-        function crear(){
-            localStorage.setItem("nombre", "Guillermo")
-            localStorage.setItem("apellido", "Chang")
-        
-        }
-
-        function mostrar(){
-            alert("Nombre: "+ localStorage.getItem("nombre") + ' ' + "Apellido: " + localStorage.getItem("apellido"))
-        }
-
-        function modificar(){
-            localStorage.setItem("nombre", "David")
-            localStorage.setItem("apellido", "Gordillo")
-        }
-
-        function eliminar(){
-            localStorage.removeItem("nombre")
-            localStorage.removeItem("apellido")
-        }
-*/
-/*
-function guardar_local(){
-    let contacto= {
-        
-    }
-}
-*/
