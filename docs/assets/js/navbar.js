b653638cda8d8eb1de23bb85ca6ecbbb58c85ff1
Ruravi-jpg <63319925+Ruravi-jpg@users.noreply.github.com>
2022-01-28 10:14:47 -0600

    updateNavbar

---
+++ docs/assets/js/navbar.js
@@ -0,0 +1,57 @@
+let navbar = document.getElementById("navbar");
+
+navbar.innerHTML += `
+
+<nav class="navbar">
+<ul class="navbar-nav">
+  <li class="logo">
+    <a href="#" class="nav-link">
+      <span class="link-text logo-text">Somos Booking</span>
+    </a>
+  </li>
+
+  <li class="nav-item">
+    <a href="./navbar.html" class="nav-link">
+      <span class="link-text">Home</span>
+    </a>
+  </li>
+
+  <li class="nav-item">
+    <a href="./nosotros.html" class="nav-link">
+     
+      <span class="link-text">¿Quiénes somos?</span>
+    </a>
+  </li>
+
+  <li class="nav-item">
+    <a href="./index.html" class="nav-link">
+    
+      <span class="link-text">Familia Somos Booking</span>
+    </a>
+  </li>
+
+  <li class="nav-item">
+    <a href="#" class="nav-link">
+    
+      <span class="link-text">Servicios</span>
+    </a>
+  </li>
+
+  <li class="nav-item">
+    <a href="#" class="nav-link">
+    
+      <span class="link-text">Eventos</span>
+    </a>
+  </li>
+  <li class="nav-item">
+    <a href="#" class="nav-link">
+    
+      <span class="link-text">Tienda</span>
+    </a>
+  </li>
+
+  
+</ul>
+</nav>
+
+`;
