d83f46e90acb6c2c435e45026966cf4bf491dda8
Ruravi-jpg <63319925+Ruravi-jpg@users.noreply.github.com>
2022-02-10 13:21:39 -0600

    actualizado navbar para que funcione con githubPages

--- docs/js/navbar.js
+++ docs/js/navbar.js
@@ -14,7 +14,7 @@ navbar.innerHTML += `
                 <a href="#" class="nav-link">
                  
                   <span class="link-text">¿Quiénes somos?</span>
-                  <img class="nav-icon" src="/docs/assets/navbarIconos/hola.png" alt="">
+                  <img class="nav-icon" src="../assets/navbarIconos/hola.png" alt="">
                 </a>
               </li>
             
@@ -22,15 +22,15 @@ navbar.innerHTML += `
                 <a href="#" class="nav-link">
                 
                   <span class="link-text">Familia Somos Booking</span>
-                  <img class="nav-icon" src="/docs/assets/navbarIconos/familiabooking2.png" alt="">
+                  <img class="nav-icon" src="../assets/navbarIconos/familiabooking2.png" alt="">
                 </a>
               </li>
             
               <li class="nav-item">
-                <a href="/docs/html/servicios.html" class="nav-link">
+                <a href="./servicios.html" class="nav-link">
                 
                   <span class="link-text">Servicios</span>
-                  <img class="nav-icon" src="/docs/assets/navbarIconos/sevicios2.png" alt="">
+                  <img class="nav-icon" src="../assets/navbarIconos/sevicios2.png" alt="">
                 </a>
               </li>
             
@@ -38,30 +38,30 @@ navbar.innerHTML += `
                 <a href="#" class="nav-link">
                 
                   <span class="link-text">Eventos</span>
-                  <img class="nav-icon" src="/docs/assets/navbarIconos/eventos2.png" alt="">
+                  <img class="nav-icon" src="../assets/navbarIconos/eventos2.png" alt="">
                 </a>
               </li>
               <li class="nav-item">
-                <a href="/docs/html/tienda.html" class="nav-link">
+                <a href="./tienda.html" class="nav-link">
                 
                   <span class="link-text">Tienda</span>
-                  <img class="nav-icon" src="/docs/assets/navbarIconos/tienda2.png" alt="">
+                  <img class="nav-icon" src="../assets/navbarIconos/tienda2.png" alt="">
                 </a>
               </li>
             
               <li class="nav-item">
-                <a href="/docs/html/contactUs.html" class="nav-link">
+                <a href="./contactUs.html" class="nav-link">
                 
                   <span class="link-text">Contacto</span>
-                  <img class="nav-icon" src="/docs/assets/navbarIconos/contacto2.png" alt="">
+                  <img class="nav-icon" src="../assets/navbarIconos/contacto2.png" alt="">
                 </a>
               </li>
             
               <li class="nav-item">
-                <a href="/docs/html/AboutUsNew.html" class="nav-link">
+                <a href="./AboutUsNew.html" class="nav-link">
                 
                   <span class="link-text">Sobre nosotros</span>
-                  <img class="nav-icon" src="/docs/assets/navbarIconos/nosotros2.png" alt="">
+                  <img class="nav-icon" src="../assets/navbarIconos/nosotros2.png" alt="">
                 </a>
               </li>
             
